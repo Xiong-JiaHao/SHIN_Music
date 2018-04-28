3269eaa3bd3c6f1a4717791aeea9bf171a2a4044
XJH <254157101@qq.com>
2018-04-28 11:44:23 +0800

    更新了底部弹出界面

--- app/src/main/java/com/gin/xjh/shin_music/adapter/musicRecyclerViewAdapter.java
+++ app/src/main/java/com/gin/xjh/shin_music/adapter/musicRecyclerViewAdapter.java
@@ -1,7 +1,9 @@
 package com.gin.xjh.shin_music.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 
 import com.gin.xjh.shin_music.R;
 import com.gin.xjh.shin_music.bean.Song;
+import com.gin.xjh.shin_music.util.DensityUtil;
 
 import java.util.List;
 
@@ -68,9 +71,22 @@ public class musicRecyclerViewAdapter extends RecyclerView.Adapter<musicRecycler
             sz.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(context, "check", Toast.LENGTH_SHORT).show();
+                    showbottomDialog();
                 }
             });
         }
+    }
+
+    private void showbottomDialog() {
+        Dialog bottomDialog = new Dialog(context, R.style.BottomDialog);
+        View contentView = LayoutInflater.from(context).inflate(R.layout.dialog_content_circle, null);
+        bottomDialog.setContentView(contentView);
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) contentView.getLayoutParams();
+        params.width = context.getResources().getDisplayMetrics().widthPixels - DensityUtil.dp2px(context, 16f);
+        params.bottomMargin = DensityUtil.dp2px(context, 8f);
+        contentView.setLayoutParams(params);
+        bottomDialog.getWindow().setGravity(Gravity.BOTTOM);
+        bottomDialog.getWindow().setWindowAnimations(R.style.BottomDialog_Animation);
+        bottomDialog.show();
     }
 }
