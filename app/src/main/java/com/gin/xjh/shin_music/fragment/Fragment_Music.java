b5cf02ca569fdcf684a8ee01f9e2e1b96c201d4a
XJH <254157101@qq.com>
2018-06-01 22:13:13 +0800

    更新CD旋转方法，可记录上次暂停位置

--- app/src/main/java/com/gin/xjh/shin_music/fragment/Fragment_Music.java
+++ app/src/main/java/com/gin/xjh/shin_music/fragment/Fragment_Music.java
@@ -59,10 +59,15 @@ public class Fragment_Music extends Fragment {
                     .into(mAlbum);
         }
 
+        //设定动画作用于的控件，以及什么动画，旋转的开始角度和结束角度
         objAnim = ObjectAnimator.ofFloat(mAlbum, "rotation", 0.0f, 360.0f);
+        //设定动画的旋转周期
         objAnim.setDuration(20000);
+        //设置动画的插值器，这个为匀速旋转
         objAnim.setInterpolator(new LinearInterpolator());
+        //设置动画为无限重复
         objAnim.setRepeatCount(-1);
+        //设置动画重复模式
         objAnim.setRepeatMode(ObjectAnimator.RESTART);
 
     }
